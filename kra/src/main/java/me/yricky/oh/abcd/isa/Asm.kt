fb68a91ab86d1af5912ff88158719c4d569d399e
yricky <student.yricky@qq.com>
2024-06-17 00:15:31 +0800

    feat:着重优化字节码解析部分，让中间产物更可用一些，别光获取一个反编译字符串

--- kra/src/main/java/me/yricky/oh/abcd/isa/Asm.kt
+++ kra/src/main/java/me/yricky/oh/abcd/isa/Asm.kt
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import me.yricky.oh.abcd.code.Code
 import me.yricky.oh.abcd.code.TryBlock
 import me.yricky.oh.abcd.isa.bean.Isa
+import me.yricky.oh.utils.DataAndNextOff
+import me.yricky.oh.utils.nextOffset
+import me.yricky.oh.utils.value
 
 class Asm(
     val code: Code,
@@ -34,6 +37,8 @@ class Asm(
         li
     }
 
+
+
     class AsmItem(
         val asm:Asm,
         val ins:Inst,
@@ -42,50 +47,57 @@ class Asm(
         val tryBlock:TryBlock? get() = asm.code.tryBlocks.firstOrNull {
             codeOffset in (it.offset until (it.offset+ it.length))
         }
+
+        val opRand by lazy {
+            val instructions = asm.code.instructions
+            val oprand = mutableListOf<Number>()
+            val iter = ins.format.iterator()
+            var off = codeOffset
+            while (iter.hasNext()){
+                val thisFmt = iter.next()
+                if(thisFmt is Inst.InstFmt.Prefix || thisFmt is Inst.InstFmt.OpCode){
+                    oprand.add(instructions.get(off))
+                    off += 1
+                } else if(thisFmt.bitSize == 4){
+                    val nextFmt = iter.next()
+                    if(nextFmt.bitSize != 4){
+                        throw IllegalStateException()
+                    }
+                    val value = instructions.get(off).toUByte().toInt()
+                    oprand.add((value and  0xf).toByte())
+                    oprand.add(value.shr(4).toByte())
+                    off += 1
+                } else if(thisFmt.bitSize == 8){
+                    val value = instructions.get(off)
+                    oprand.add(value)
+                    off += 1
+                } else if(thisFmt.bitSize == 16){
+                    val value = instructions.getShort(off)
+                    oprand.add(value)
+                    off += 2
+                } else if(thisFmt.bitSize == 32){
+                    val value = instructions.getInt(off)
+                    oprand.add(value)
+                    off += 4
+                } else if(thisFmt.bitSize == 64){
+                    val value = instructions.getLong(off)
+                    oprand.add(value)
+                    off += 8
+                } else throw IllegalStateException("Unsupported bitSize")
+            }
+            DataAndNextOff(oprand,off)
+        }
+
         val disassembleString:String by lazy {
             val sb = StringBuilder()
             val initOff = codeOffset
-            var off = codeOffset //opCode之后的
 
             with(asm.code){
-                val oprand = mutableListOf<Number>()
-                val iter = ins.format.iterator()
-                while (iter.hasNext()){
-                    val thisFmt = iter.next()
-                    if(thisFmt is Inst.InstFmt.Prefix || thisFmt is Inst.InstFmt.OpCode){
-                        off += 1
-                    } else if(thisFmt.bitSize == 4){
-                        val nextFmt = iter.next()
-                        if(nextFmt.bitSize != 4){
-                            throw IllegalStateException()
-                        }
-                        val value = instructions.get(off).toUByte().toInt()
-                        oprand.add(value and  0xf)
-                        oprand.add(value.shr(4))
-                        off += 1
-                    } else if(thisFmt.bitSize == 8){
-                        val value = instructions.get(off)
-                        oprand.add(value)
-                        off += 1
-                    } else if(thisFmt.bitSize == 16){
-                        val value = instructions.getShort(off).toUShort()
-                        oprand.add(value.toInt())
-                        off += 2
-                    } else if(thisFmt.bitSize == 32){
-                        val value = instructions.getInt(off)
-                        oprand.add(value)
-                        off += 4
-                    } else if(thisFmt.bitSize == 64){
-                        val value = instructions.getLong(off)
-                        oprand.add(value)
-                        off += 8
-                    } else throw IllegalStateException()
-                }
-                sb.append(ins.asmString(asm.code,oprand))
+                sb.append(ins.asmString(asm.code,opRand.value))
             }
             sb.append(" ".repeat((8 - sb.length%8)))
             sb.append("//")
-            (initOff until off).forEach {
+            (initOff until opRand.nextOffset).forEach {
                 sb.append(String.format("%02X",asm.code.instructions.get(it)))
             }
             sb.toString()
--- kra/src/main/java/me/yricky/oh/abcd/isa/Inst.kt
+++ kra/src/main/java/me/yricky/oh/abcd/isa/Inst.kt
@@ -11,11 +11,15 @@ import me.yricky.oh.utils.value
 class Inst(
     val group:InsGroup,
     val instruction: Instruction,
-    val opCode:Byte,
-    val format:List<InstFmt>
+    private val index:Int
 ) {
     private val sigSplit by lazy {
         instruction.sig.split(' ').map { it.removeSuffix(",") }
+    }
+
+    val opCode get() = instruction.opcodeIdx[index]
+    val format:List<InstFmt> by lazy {
+        InstFmt.fromString(instruction.format[index],sigSplit)
     }
 
     fun argSize():Int{
@@ -27,35 +31,42 @@ class Inst(
     }
 
     val asmName get() = sigSplit[0]
-    val fmtString:String get() = instruction.format[instruction.opcodeIdx.indexOf(opCode)]
 
     fun asmString(code: Code,args:List<Number>):String{
         val sb = StringBuilder()
         sb.append(asmName)
         sb.append(' ')
         with(code){
-            (1 until sigSplit.size).forEach {
-                val argSig = sigSplit[it]
-                when{
-                    argSig.startsWith("imm") -> sb.append(args[it - 1]).append(' ')
-                    argSig.startsWith("v") -> sb.append('v').append(args[it - 1]).append(' ')
-                    argSig == "method_id" -> {
-                        val value = args[it - 1].toInt().let { m.region.mslIndex[it] }
+            args.indices.forEach {
+                when(val argSig = format[it]){
+                    is InstFmt.OpCode,is InstFmt.Prefix -> {}
+                    is InstFmt.ImmI -> sb.append(args[it]).append(' ')
+                    is InstFmt.ImmU -> when(val arg = args[it]){
+                        is Byte -> sb.append(arg.toUByte()).append(' ')
+                        is Short -> sb.append(arg.toUShort()).append(' ')
+                        is Int -> sb.append(arg.toUInt()).append(' ')
+                        is Long -> sb.append(arg.toULong()).append(' ')
+                    }
+                    is InstFmt.ImmF -> when(val arg = args[it]){
+                        is Int -> sb.append(Float.fromBits(arg)).append(' ')
+                        is Long -> sb.append(Double.fromBits(arg)).append(' ')
+                        else -> throw IllegalStateException("invalid float bitSize:${argSig.bitSize}")
+                    }
+                    is InstFmt.RegV -> sb.append('v').append(args[it]).append(' ')
+                    is InstFmt.MId -> {
+                        val value = args[it].toUnsignedInt().let { m.region.mslIndex[it] }
                         val method = if(m.abc.isForeignOffset(value)) ForeignMethod(m.abc,value) else AbcMethod(m.abc,value)
                         sb.append("${method.clazz.name}.${method.name} ")
                     }
-                    argSig == "literalarray_id" -> {
-                        val value = args[it - 1].toInt()
+                    is InstFmt.LId -> {
+                        val value = args[it].toUnsignedInt()
                         val literalArray = LiteralArray(m.abc,m.region.mslIndex[value])
                         sb.append("$literalArray ")
                     }
-                    argSig == "string_id" -> {
-                        val value = args[it - 1].toInt()
+                    is InstFmt.SId -> {
+                        val value = args[it].toUnsignedInt()
                         val str = m.abc.stringItem(m.region.mslIndex[value])
                         sb.append("\"${str.value}\" ")
-                    }
-                    else -> {
-                        sb.append("(${argSig})${args[it-1]} ")
                     }
                 }
             }
@@ -71,44 +82,73 @@ class Inst(
             return Inst(
                 group,
                 instruction,
-                instruction.opcodeIdx[index],
-                InstFmt.fromString(instruction.format[index])
+                index
             )
+        }
+
+        fun Number.toUnsignedInt():Int = when(this){
+            is Byte -> toUByte().toInt()
+            is Short -> toUShort().toInt()
+            is Int -> {
+                if(this < 0){
+                    throw IllegalStateException("$this < 0")
+                } else this
+            }
+            is Long -> if(this < 0 || this > 0x7fff_ffffL){
+                throw IllegalStateException("$this < 0")
+            } else this.toInt()
+            else -> throw IllegalStateException("unsupported")
         }
     }
 
     sealed class InstFmt(val bitSize:Int){
         companion object{
-            fun fromString(str:String):List<InstFmt>{
-                val strListIter = str.split('_').iterator()
+            fun fromString(fmt:String,sigSplit:List<String>):List<InstFmt>{
+                val strListIter = fmt.split('_').iterator()
+                val sigIter = sigSplit.iterator().apply { /* 跳过name段 */ next() }
                 val res = mutableListOf<InstFmt>()
                 while (strListIter.hasNext()){
                     val thisFmt = strListIter.next()
                     val instFmt = when{
                         thisFmt == "pref" -> Prefix()
                         thisFmt == "op" -> OpCode()
+                        thisFmt == "none" -> null
                         thisFmt.startsWith("imm") -> {
+                            val thisSig = sigIter.next()
                             val bitSize = strListIter.next().toInt()
                             val index = if(thisFmt.length == 4){
                                 thisFmt.last().code - '0'.code
                             } else null
-                            Imm(index, bitSize)
+                            when{
+                                thisSig.contains(":i") -> ImmI(index, bitSize)
+                                thisSig.contains(":u") -> ImmU(index, bitSize)
+                                thisSig.contains(":f") -> ImmF(index, bitSize)
+                                else -> throw IllegalStateException("invalid imm sig:$thisSig")
+                            }
+
                         }
                         thisFmt.startsWith("id") -> {
+                            val thisSig = sigIter.next()
                             val bitSize = strListIter.next().toInt()
                             val index = if(thisFmt.length == 3){
                                 thisFmt.last().code - '0'.code
                             } else null
-                            Id(index, bitSize)
+                            when(thisSig){
+                                "method_id" -> MId(index, bitSize)
+                                "literalarray_id" -> LId(index, bitSize)
+                                "string_id" -> SId(index, bitSize)
+                                else -> throw IllegalStateException("no such id type:$thisSig")
+                            }
                         }
                         thisFmt.startsWith("v") -> {
+                            val thisSig = sigIter.next()
                             val bitSize = strListIter.next().toInt()
                             val index = if(thisFmt.length == 2){
                                 thisFmt.last().code - '0'.code
                             } else null
                             RegV(index, bitSize)
                         }
-                        else -> null
+                        else -> throw IllegalStateException("no such fmt:${thisFmt}")
                     }
                     if(instFmt != null){
                         res.add(instFmt)
@@ -123,8 +163,12 @@ class Inst(
         /**
          * @param index 0表示只有这一个这个类型的操作数
          */
-        class Imm(val index:Int? = 0, bitSize:Int):InstFmt(bitSize)
+        class ImmI(val index:Int? = 0, bitSize:Int):InstFmt(bitSize)
+        class ImmU(val index:Int? = 0, bitSize:Int):InstFmt(bitSize)
+        class ImmF(val index:Int? = 0, bitSize:Int):InstFmt(bitSize)
         class RegV(val index: Int? = 0, bitSize: Int):InstFmt(bitSize)
-        class Id(val index: Int? = 0, bitSize: Int):InstFmt(bitSize)
+        class SId(val index: Int? = 0, bitSize: Int):InstFmt(bitSize)
+        class MId(val index: Int? = 0, bitSize: Int):InstFmt(bitSize)
+        class LId(val index: Int? = 0, bitSize: Int):InstFmt(bitSize)
     }
 }
