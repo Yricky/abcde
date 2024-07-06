0ca25f1425b26bb55d783454be8be0b9ea59265b
yricky <student.yricky@qq.com>
2024-07-06 17:44:52 +0800

    fix:修复安卓上部分java17方法无法使用的问题

--- modules/common/src/jvmMain/kotlin/me/yricky/oh/common/misc.kt
+++ modules/common/src/jvmMain/kotlin/me/yricky/oh/common/misc.kt
@@ -3,10 +3,13 @@ package me.yricky.oh.common
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-fun wrapAsLEByteBuf(buffer: ByteBuffer): LEByteBuf {
+fun wrapAsLEByteBuf(_buffer: ByteBuffer): LEByteBuf {
+    val buffer = if(_buffer.order() != ByteOrder.LITTLE_ENDIAN){
+        _buffer.order(ByteOrder.LITTLE_ENDIAN)
+    } else _buffer
     return object : LEByteBuf {
         override fun get(index: Int, dst: ByteArray){
-            buffer.get(index, dst)
+            buffer.slice(index,dst.size).order(ByteOrder.LITTLE_ENDIAN).get(dst)
         }
 
         override fun get(index: Int): Byte = buffer.get(index)
