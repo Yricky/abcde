6b65af4d3c37a96be0a1c228c035a54c27d3cccf
yricky <student.yricky@qq.com>
2024-09-26 22:37:14 +0800

    feat:添加汇编中res读取（未测试）

--- modules/abcde/src/commonMain/kotlin/me/yricky/oh/abcd/isa/Asm.kt
+++ modules/abcde/src/commonMain/kotlin/me/yricky/oh/abcd/isa/Asm.kt
@@ -167,3 +167,32 @@ val Asm.AsmItem.calledStrings:Sequence<String> get() = sequence {
         }
     }
 }
+
+
+val Asm.AsmItem.calledResources:Sequence<Int> get() = sequence {
+    ins.format.forEachIndexed { index, instFmt ->
+        if(instFmt is InstFmt.LId){
+            val value = opUnits[index].toUnsignedInt()
+            val literalArray = asm.code.abc.literalArray(asm.code.method.region.mslIndex[value])
+            if(literalArray.content.size % 2 == 0){
+                val objKv = mutableMapOf<String,LiteralArray.Literal>()
+                val iter = literalArray.content.iterator()
+                while (iter.hasNext()){
+                    val next = iter.next()
+                    if(next is LiteralArray.Literal.Str){
+                        objKv[next.get(asm.code.abc)] = iter.next()
+                    } else break
+                }
+                if(
+                    objKv["id"] is LiteralArray.Literal.I32 &&
+                    objKv["type"] is LiteralArray.Literal.I32 &&
+                    objKv["params"] != null &&
+                    objKv["bundleName"] is LiteralArray.Literal.Str &&
+                    objKv["moduleName"] is LiteralArray.Literal.Str
+                ){
+                    yield((objKv["id"] as LiteralArray.Literal.I32).value)
+                }
+            }
+        }
+    }
+}
