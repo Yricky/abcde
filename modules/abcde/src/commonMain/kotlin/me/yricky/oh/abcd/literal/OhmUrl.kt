6f41467b32c5f968e068d66528ecee98e678360f
yricky <student.yricky@qq.com>
2025-05-10 23:12:21 +0800

    feat(abcde):优化OhmUrl解析

--- modules/abcde/src/commonMain/kotlin/me/yricky/oh/abcd/literal/OhmUrl.kt
+++ modules/abcde/src/commonMain/kotlin/me/yricky/oh/abcd/literal/OhmUrl.kt
@@ -12,12 +12,23 @@ value class OhmUrl(val str:String) {
         const val PREFIX_NATIVE_LIB = "@app:"
         const val PREFIX_NATIVE_SYS = "@native:"
         const val PREFIX_OHOS = "@ohos:"
+        const val PREFIX_HMS = "@hms:"
+        const val PREFIX_NORMALIZED = "@normalized:"
+        const val PREFIX_NORMALIZED_SO = "@normalized:Y"
+        const val PREFIX_NORMALIZED_NOT_CROSS_HAP_FILE = "@normalized:N&&&"
     }
     override fun toString(): String {
         return str
     }
 
-    fun isAbcModule():Boolean = str.startsWith(PREFIX_MODULE) || str.startsWith(PREFIX_THIRD_PARTY)
+    /**
+     * 这些方法暂时不保证准确
+     */
+    fun isAbcModule():Boolean = str.startsWith(PREFIX_MODULE) ||
+            str.startsWith(PREFIX_THIRD_PARTY) ||
+            str.startsWith(PREFIX_NORMALIZED_NOT_CROSS_HAP_FILE)
     fun isSystemModule():Boolean = str.startsWith(PREFIX_OHOS)
-    fun isNativeModule():Boolean = str.startsWith(PREFIX_NATIVE_LIB)
+    fun isNativeModule():Boolean = str.startsWith(PREFIX_NATIVE_LIB) ||
+            str.startsWith(PREFIX_NORMALIZED_SO)
+    fun isNormalizedUrl():Boolean = str.startsWith(PREFIX_NORMALIZED)
 }
